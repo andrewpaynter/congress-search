6eb5772d01bca5f97a303be27d405cdb73abb7f2
Andrew Paynter <andrewpaynter97@gmail.com>
2022-09-21 12:15:49 -0400

    acao

--- src/services/CongressService.ts
+++ src/services/CongressService.ts
@@ -11,7 +11,12 @@ class CongressService {
   }
   async getCongressData(chartData: ChartData) {
     let url: string = `https://congress-searcher-api.herokuapp.com/api/congress?${stringify(chartData)}`
-    const response = await axios.get<Congressperson[]>(url)
+    const config = {
+      headers: {
+        "Access-Control-Allow-Origin" : 'https://congress-searcher.herokuapp.com/'
+      }
+    }
+    const response = await axios.get<Congressperson[]>(url, config)
     this.finalPage = response.headers.finalitem === 'true'
     return response.data
   }
