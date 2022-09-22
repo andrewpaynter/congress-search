c0c63586f8cf68ff715cad13dabcdbd53b08cbab
Andrew Paynter <andrewpaynter97@gmail.com>
2022-09-22 11:52:28 -0400

    bugfix incorrect order on chart

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -74,13 +74,13 @@ export const Table = ({congressData, chartData, setChartData}: TableProps) => {
         <tbody>
           {congressData.map((cp: Congressperson) => 
           <tr className='p-4 border-b border-slate-200 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 items-center' key={cp.id}>
-            <td>
+            <td className='hidden lg:block'>
               <img 
               src= {`https://congress-searcher-api.herokuapp.com/api/img/${cp.id}`}
               alt={cp.name}
-              className='h-32 hidden lg:block'
+              className='h-32'
               />
-              </td>
+            </td>
             <td>{cp.name}</td>
             <td>{cp.party}</td>
             <td>{cp.state}</td>
